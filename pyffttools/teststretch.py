033bd89604cd19259d1ac610aba8447beac8b472
Dmitry Protopopov <protopopov@narod.ru>
2018-09-08 22:11:41 +0300

    StretchBuilder

--- pyffttools/teststretch.py
+++ pyffttools/teststretch.py
@@ -5,7 +5,9 @@ import cv2
 img = cv2.imread('~\\..\\Physicians.jpg')
 height, width, channels = img.shape
 data = np.asarray(img, float)/255
-builder = StretchBuilder((int)(height*2), (int)(width*2))
-cv2.imshow('image', builder.Stretch(data))
+builder1 = StretchBuilder((int)(height*2), (int)(width*2))
+builder2 = StretchBuilder((int)(height/2), (int)(width/2))
+cv2.imshow('inc', builder1.Stretch(data))
+cv2.imshow('dec', builder2.Stretch(data))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
